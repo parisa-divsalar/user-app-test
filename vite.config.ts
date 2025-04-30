22800ee3c177f4bda64bd291a154545f2c52a87f
r.hatampoor <rezahatampoor9@gmail.com>
2025-04-30 14:08:21 +0330

    fix:fixed build issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
     port: 3000, // Change the port number here
   },
@@ -16,7 +14,3 @@ export default defineConfig({
     manifest: true,
   },
 });
-function tailwindcss(): import("vite").PluginOption {
-  throw new Error('Function not implemented.');
-}
-
