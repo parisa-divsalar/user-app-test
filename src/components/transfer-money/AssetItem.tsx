d9149c38278e9354453db3a775119bb7e8754880
r.hatampoor <rezahatampoor9@gmail.com>
2025-05-04 23:40:11 +0330

    fix:fixed

--- src/components/transfer-money/AssetItem.tsx
+++ src/components/transfer-money/AssetItem.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { AssetsList } from './AssetsList';
 import useStyles from './useStyles';
+import { GoldenMackIcon } from '../UI/Icons/transferMoney/GoldenMackIcon';
+import { SavingMackIcon } from '../UI/Icons/transferMoney/SavingMackIcon';
 
 export const AssetItem: FC<AssetsList> = ({
   backgroundColor,
@@ -8,10 +10,13 @@ export const AssetItem: FC<AssetsList> = ({
   progressValue,
   title,
   uniteNumber,
+  id,
+  maskIcon
 }) => {
   const { assetItem, assetTitle, progressBar, progressValueStyle } = useStyles();
+
   return (
-    <div style={{ backgroundColor: backgroundColor }} className={assetItem}>
+    <div style={{ backgroundColor: backgroundColor,opacity:id!==1?'0.4':'1' }} className={assetItem}>
       <h2 className={assetTitle}>{title}</h2>
       <p style={{ padding: '0', margin: '0' }}> تعداد واحد : {uniteNumber}</p>
       <p style={{ padding: '0', margin: '0' }}>{inventory}</p>
